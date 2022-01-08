73a8f42ed241c085f051d30b8d78b45d45ebd944
unknown <mutiyah.kem@gmail.com>
2022-01-08 23:40:53 +0100

    first commit

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,22 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent implements OnInit {
-project1 : string = "assets/images/project1.jpeg";
-project2: string = "assets/images/project2.jpeg";
-project3: string = "assets/images/project3.jpeg";
-project4: string = "assets/images/project4.jpeg";
-reactProject1 : string = "assets/images/reactProject1.jpg";
-reactProject2 : string = "assets/images/netflix.jpg";
-reactProject3 : string = "assets/images/linkedin.jpg";
+  project1: string = 'assets/images/project1.jpeg';
+  project2: string = 'assets/images/project2.jpeg';
+  project3: string = 'assets/images/project3.jpeg';
+  project4: string = 'assets/images/project4.jpeg';
+  reactProject1: string = 'assets/images/reactProject1.jpg';
+  reactProject2: string = 'assets/images/netflix.jpg';
+  reactProject3: string = 'assets/images/linkedin.jpg';
+  reactProject4: string = 'assets/images/chitProject.jpg';
 
+  constructor() {}
 
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
